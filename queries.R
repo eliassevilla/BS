85170bc20f2a240c647beda9bfebfb4a8adecdb6
Elias Sevilla <eliassevilla@ciencias.unam.mx>
2021-01-28 02:15:34 +0000

    # Reto 1 Sesión 7

---
+++ queries.R
@@ -0,0 +1,34 @@
+# RETO 1 
+
+install.packages("dplyr")
+install.packages("DBI")
+install.packages("RMySQL")
+install.packages("ggplot2")
+
+library(dplyr)
+library(DBI)
+library(RMySQL)
+library(ggplot2)
+
+MyDataBase <- dbConnect(
+  drv = RMySQL::MySQL(),
+  dbname = "shinydemo",
+  host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+  username = "guest",
+  password = "guest")
+
+dbListTables(MyDataBase)
+
+dbListFields(MyDataBase, 'CountryLanguage')
+
+DataDB <- dbGetQuery(MyDataBase, "select * from CountryLanguage")
+
+head(DataDB)
+
+SP <- DataDB %>% filter(Language == "Spanish")
+class(SP)
+
+SP %>% ggplot(aes( x = CountryCode, y = Percentage, fill = IsOfficial )) + 
+  geom_bin2d() +
+  coord_flip()
+
